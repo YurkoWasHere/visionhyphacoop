595abeba0f4dc82da68671c0d2426374b65d27cb
YurkoWasHere <39072689+YurkoWasHere@users.noreply.github.com>
2019-05-27 12:35:09 -0400

    Corrected indentations

--- upload.php
+++ upload.php
@@ -6,9 +6,9 @@ $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 if(isset($_POST["submit"])) {
     $check = getimagesize($_FILES["uploadFile"]["tmp_name"]);
     if($check !== false) {
-		$fileparts=explode(".",$_FILES["uploadFile"]["name"]);
+	$fileparts=explode(".",$_FILES["uploadFile"]["name"]);
         $fileName= date("Ymd-His") . "." .  $fileparts[count($fileparts)-1];
-		move_uploaded_file($_FILES["uploadFile"]["tmp_name"],"/home/hyphacoop/www/files/" . $fileName);
+	move_uploaded_file($_FILES["uploadFile"]["tmp_name"],"/home/hyphacoop/www/files/" . $fileName);
     } else {
         echo "File is not an image.!!";
         $uploadOk = 0;
